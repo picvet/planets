6b5c2711272d25c9c47296947cd75aebdbf80bf2
Percy Zulu <percyzulu2@outlook.com>
2026-02-07 09:32:31 +0200

    chore:  update imports for base models

--- alembic/env.py
+++ alembic/env.py
@@ -14,7 +14,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-from resources.database.models import base_metadata
+from src.resources.database.models import base_metadata
 
 # add your model's MetaData object here
 # for 'autogenerate' support
