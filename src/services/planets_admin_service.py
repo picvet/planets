939cda7abbf95000c67851046f3b0f66b6c17edb
Percy Zulu <percyzulu2@outlook.com>
2026-02-08 18:46:51 +0200

    feat: implement create cargo type
    - service method
    - test suite
    - removed unnecessary try.. catch

--- src/services/planets_admin_service.py
+++ src/services/planets_admin_service.py
@@ -6,6 +6,8 @@ from src.generated.co.za.planet import (
     StatusCode,
     CreatePlanetRequest,
     CreatePlanetResponse,
+    CreateCargoTypeResponse,
+    CreateCargoTypeRequest,
 )
 from src.resources.database.config import Session
 
@@ -17,31 +19,23 @@ from src.strings import en_za as strings
 class PlanetsService(PlanetAdminBase):
 
     async def create_sector(self, create_sector_request: CreateSectorRequest) -> CreateSectorResponse:
-        try:
-            async with Session() as session:
-                if not create_sector_request.sector_name:
-                    return CreateSectorResponse(
-                        message=ResponseMessage(
-                            status_code=StatusCode.VALIDATION_ERROR,
-                            error_fields={"sector_name": strings.validation_error_required_field},
-                        )
-                    )
-
-                sector = models.Sector(name=create_sector_request.sector_name)
-                session.add(sector)
-                await session.flush()
-                await session.commit()
-
+        async with Session() as session:
+            if not create_sector_request.sector_name:
                 return CreateSectorResponse(
-                    message=ResponseMessage(status_code=StatusCode.SUCCESS),
-                    sector_id=sector.sector_id,
+                    message=ResponseMessage(
+                        status_code=StatusCode.VALIDATION_ERROR,
+                        error_fields={"sector_name": strings.validation_error_required_field},
+                    )
                 )
 
-        except Exception:
-            import traceback
+            sector = models.Sector(name=create_sector_request.sector_name)
+            session.add(sector)
+            await session.commit()
 
-            traceback.print_exc()
-            raise
+            return CreateSectorResponse(
+                message=ResponseMessage(status_code=StatusCode.SUCCESS),
+                sector_id=sector.sector_id,
+            )
 
     async def create_planet(self, create_planet_request: "CreatePlanetRequest") -> "CreatePlanetResponse":
         async with Session() as session:
@@ -61,3 +55,21 @@ class PlanetsService(PlanetAdminBase):
                 return CreatePlanetResponse(message=ResponseMessage(status_code=StatusCode.INTERNAL_ERROR))
 
             return CreatePlanetResponse(message=ResponseMessage(status_code=StatusCode.SUCCESS), planet_id=planet.planet_id)
+
+    async def create_cargo_type(self, create_cargo_type_request: "CreateCargoTypeRequest") -> "CreateCargoTypeResponse":
+        async with Session() as session:
+            if not create_cargo_type_request.cargo_name:
+                return CreateCargoTypeResponse(
+                    message=ResponseMessage(
+                        status_code=StatusCode.VALIDATION_ERROR,
+                        error_fields={"cargo_name": strings.validation_error_required_field},
+                    ),
+                )
+
+            cargo = models.CargoType(name=create_cargo_type_request.cargo_name)
+            session.add(cargo)
+            await session.commit()
+            return CreateCargoTypeResponse(
+                message=ResponseMessage(status_code=StatusCode.SUCCESS),
+                cargo_type_id=cargo.cargo_type_id,
+            )
--- tests/test_planet_service.py
+++ tests/test_planet_service.py
@@ -1,33 +1,66 @@
 import pytest
 from grpclib.testing import ChannelFor
 
-from src.generated.co.za.planet import CreateSectorRequest, PlanetAdminStub, StatusCode, CreatePlanetRequest
+from src.generated.co.za.planet import (
+    CreateSectorRequest,
+    PlanetAdminStub,
+    StatusCode,
+    CreatePlanetRequest,
+    CreateCargoTypeRequest,
+)
+
+from src.strings import en_za as strings
 
 
 @pytest.mark.asyncio
 async def test_create_planet(planets_service):
     async with ChannelFor([planets_service]) as channel:
         stub = PlanetAdminStub(channel)
-        sector_name = "Orion Nebula"
-        request = CreateSectorRequest(sector_name=sector_name)
-        response = await stub.create_sector(request)
-        assert response.message.status_code == StatusCode.SUCCESS
+        planet_1 = "Planet 1"
+        planet_2 = "Planet 2"
+        sector_name = "Sector 1"
+        cargo_name = "Cargo 1"
+
+        create_sector_empty = await stub.create_sector(CreateSectorRequest())
+        assert create_sector_empty.message.status_code == StatusCode.VALIDATION_ERROR
+        assert create_sector_empty.message.error_fields == {"sector_name": strings.validation_error_required_field}
+
+        create_sector_success = await stub.create_sector(CreateSectorRequest(sector_name=sector_name))
+        assert create_sector_success.message.status_code == StatusCode.SUCCESS
+
+        create_empty_cargo_response = await stub.create_cargo_type(CreateCargoTypeRequest())
+        assert create_empty_cargo_response.message.status_code == StatusCode.VALIDATION_ERROR
+
+        create_cargo_type_success = await stub.create_cargo_type(CreateCargoTypeRequest(cargo_name=cargo_name))
+        assert create_cargo_type_success.message.status_code == StatusCode.SUCCESS
 
         create_empty_planet_response = await stub.create_planet(CreatePlanetRequest())
         assert create_empty_planet_response.message.status_code == StatusCode.VALIDATION_ERROR
 
-        create_planet_response_success = await stub.create_planet(
+        # planet with not cargo type
+        create_planet_success_response = await stub.create_planet(
             CreatePlanetRequest(
-                planet_name="Planet 1",
+                planet_name=planet_1,
                 sector_name=sector_name,
             )
         )
-        assert create_planet_response_success.message.status_code == StatusCode.SUCCESS
+        assert create_planet_success_response.message.status_code == StatusCode.SUCCESS
 
+        # planet with nonexisting sector
         create_planet_response_error = await stub.create_planet(
             CreatePlanetRequest(
-                planet_name="Planet 1",
+                planet_name=planet_1,
                 sector_name="Not found",
             )
         )
         assert create_planet_response_error.message.status_code == StatusCode.INTERNAL_ERROR
+
+        # planet with a scarce cargo
+        create_planet_success_response_1 = await stub.create_planet(
+            CreatePlanetRequest(
+                planet_name=planet_2,
+                sector_name=sector_name,
+                scarce_cargo_name=cargo_name,
+            )
+        )
+        assert create_planet_success_response_1.message.status_code == StatusCode.SUCCESS
